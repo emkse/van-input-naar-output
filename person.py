a5bf00b1e756d8a105f0564ebb25cf6d73e9ab5c
emkse <99069595@mydavinci.nl>
2021-09-13 12:23:47 +0200

    person bestanden

---
+++ person.py
@@ -0,0 +1,10 @@
+
+print("naaminvoer")
+naaminvoer = input("emre")
+print("adresinvoer")
+adresinvoer = input("leerparkpremonade 100")
+postcodeinvoer = input ("3312 KW, dordrecht")
+print ("woonplaatsinvoer")
+woonplaatsinvoer = input ("dordrecht")
+
+
